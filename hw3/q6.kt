859146bea4cb519cfcaf94772880903ab85ed1be
Selçuk <cihanselcukoner@gmail.com>
2023-08-06 23:02:36 +0300

    update hw3/q6

--- hw3/q6.kt
+++ hw3/q6.kt
@@ -1,6 +1,8 @@
 package com.selcukoner.homeworks.hw3
 
-import com.selcukoner.denemeler.ders5.pow
+import java.lang.Math.pow
+import kotlin.math.pow
+
 
 fun main() = runIsHardyRamanujan()
 
@@ -19,14 +21,13 @@ private fun isHardyRamanujan(num:Int):Boolean
     var b = 0
     var c = 0
     var d = 0
-
     var firstPairFound = false
     outerLoop@
     for (i in 0..num){
-        if(pow(i,3) >= num) break
+        if(i.toDouble().pow(3.0) >= num) break
         for(j in 0..num){
-            if(pow(i,3) + pow(j,3) > num) break
-            if(pow(i,3) + pow(j,3) == num && i != j ){
+            if(i.toDouble().pow(3.0) + j.toDouble().pow(3.0) > num) break
+            if(i.toDouble().pow(3.0) + j.toDouble().pow(3.0) == num.toDouble() && i != j ){
                 firstPairFound = true
                 a = i
                 b = j
@@ -39,12 +40,12 @@ private fun isHardyRamanujan(num:Int):Boolean
     var secondPairFound = false
     outerLoop@
     for(i in a+1..num){
-        if(pow(i,3) >= num) break
+        if(i.toDouble().pow(3.0) >= num) break
         if (i == b) continue
         for(j in 0..num){
-            if(pow(i,3) + pow(j,3) > num) break
+            if(i.toDouble().pow(3.0) + j.toDouble().pow(3.0) > num) break
             if(j == i || j == a || j ==b) continue
-            if(pow(i,3) + pow(j,3) == num){
+            if(i.toDouble().pow(3.0) + j.toDouble().pow(3.0) == num.toDouble()){
                 secondPairFound = true
                 c = i
                 d = j
